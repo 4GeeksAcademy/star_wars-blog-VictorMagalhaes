0486da44c57eb57f39d818995e0d56d9dda40f82
VictorMagaCBT <126924738+VictorMagaCBT@users.noreply.github.com>
2023-07-02 14:59:03 +0000

    firt commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
-};
+    return (
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+				<Link to="/">
+					<img src="https://www.freepnglogos.com/uploads/star-wars-logo-31.png" width="200" alt="star wars logo" height="24" class="d-inline-block align-text-top"/>
+                    Bootstrap
+                </Link>
+				<br/>
+				<Link to="/">
+					<div class="dropdown">
+						<button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+							
+						</button>
+						<ul class="dropdown-menu">
+							<li><a class="dropdown-item" href="#">Action</a></li>
+							<li><a class="dropdown-item" href="#">Another action</a></li>
+							<li><a class="dropdown-item" href="#">Something else here</a></li>
+						</ul>
+					</div>
+						<button className="btn btn-primary"></button>
+                </Link>
+				
+            </div>
+        </nav>
+    )
+}
